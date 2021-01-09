7fb44e0be91ef67949ed040a18ed93a6bd9ba8a2
ej11240 <hwangej1124@gmail.com>
2021-01-09 18:40:17 +0900

    네비게이션 완성로그인빼고

--- MOA/client/src/App.js
+++ MOA/client/src/App.js
@@ -8,20 +8,29 @@ import LandingPage from "./LandingPage/LandingPage.js";
 import RegisterPage from "./RegisterPage/RegisterPage.js";
 import LoginPage from "./LoginPage/LoginPage";
 import Auth from "./hoc/auth";
+import Navbar from "./NavBar/Navbar";
 import Axios from 'axios'
 
 function App() {
   return (
-    <div>
+    <Suspense fallback={(<div>Loading...</div>)}>
+      <Navbar />
+      <div style={{ paddingTop: '69px' }}>
       <Switch>
-          <Route exact path="/" component={Auth(MainPage, null)} />
-          <Route exact path="/login" component={Auth(LoginPage, false)} />
-          <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          <Route exact path="/upload" component={Auth(VideoUploadPage, null)} />
-          <Route exact path="/image" component={Auth(ImageUploadPage, true)} />
-          <Route exact path="/video/:videoId" component={Auth(VideoDetailPage, null)} />
+        <Route exact path="/" component={Auth(MainPage, null)} />
+        <Route exact path="/login" component={Auth(LoginPage, false)} />
+        <Route exact path="/register" component={Auth(RegisterPage, false)} />
+        {/* <Route exact path="/broadcast" component={Auth(BroadCastPage, null)} /> */}
+        {/* <Route exact path="/broadcastform" component={Auth(BroadCastForm, null)} /> */}
+        {/* <Route exact path="/liveVideo/:liveId" component={Auth(LiveVideoPage, null)} /> */}
+        <Route exact path="/video/upload" component={Auth(VideoUploadPage, null)} />
+        {/* <Route exact path="/image/upload" component={Auth(ImageUploadPage, null)} /> */}
+        <Route exact path="/video/:videoId" component={Auth(VideoDetailPage, null)} />
+        {/* <Route exact path="/carousel" component={Carouseltest} /> */}
       </Switch>
-    </div>
+      </div>
+    </Suspense>
+    
   );
 }
 
---
+++ MOA/client/src/NavBar/Navbar.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import {Link} from 'react-router-dom';
+import { useSelector } from "react-redux";
+import {SidebarData} from'./SidebarData';
+import axios from 'axios';
+import './Navbar.css';
+import {IconContext} from 'react-icons';
+import moalogo from '../mainpage/pictures/moa.png';
+import { withRouter } from 'react-router-dom';
+
+function Navbar(props){
+
+    
+    const user = useSelector(state => state.user);
+
+    const [sidebar, setSidebar]=useState(false);
+    const showSidebar=()=> setSidebar(!sidebar);
+
+    const handleLogout = () => {
+        axios.get('/api/user/logout')
+            .then(res => {
+                if (res.status === 200) {
+                    props.history.push('/login')                        
+                } else {
+                    alert('로그아웃에 실패했습니다.');
+                }
+            })
+    }
+
+    return(
+        <div style={{ position: 'fixed', zIndex: 5, width: '100%', display: 'flex', background: 'white' }}>
+        <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+            <Link to="#" className='menu-bars'>
+                <FaIcons.FaBars onClick={showSidebar} color="#FFA625"/>
+            </Link>
+            <a href='/'><img className="logo" src={moalogo} alt="logo" /></a>
+            {
+                (user.userData && !user.userData.isAuth) ?
+                <Link to='/login' style={{ marginLeft: '570%', marginBottom: '20%'}}><button  className="loginbutton" >login</button></Link> :
+                <button style={{marginLeft:'90%'}} className="loginbutton" onClick={handleLogout}>logout</button>
+            }
+        
+            {/* <Link to='/login'><button className="loginbutton">login</button></Link>  */}
+            
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+            <ul className='nav-menu-items' onClick={showSidebar}>
+                <li className="navbar-toggle">
+                    <Link to="#" className='menu-bars'>
+                    <AiIcons.AiOutlineClose style={{color:"#F3868D"}}/>
+                    </Link>
+                </li>
+                {SidebarData.map((item,index) =>{
+                    return(
+                        <li style={{height:"80px"}} key={index} className={item.cName}>
+                            <Link to={item.path}>
+                                {item.icon}
+                                <span>{item.title}</span>
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
+        </IconContext.Provider>
+        </div>
+    );
+}
+
+export default withRouter(Navbar);
