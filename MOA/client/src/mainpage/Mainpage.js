bc0ca6b8fa7e5b9b820346ab6835dec80a267e00
ej11240 <hwangej1124@gmail.com>
2021-01-09 10:41:17 +0900

    메인 페이지 서치 프론트

---
+++ MOA/client/src/mainpage/Mainpage.js
@@ -0,0 +1,13 @@
+import React, { Component, useState } from "react";
+import * as AiIcons from "react-icons/ai";
+import * as BiIcons from "react-icons/bi";
+import MainSearchPage from './Sections/MainSearchPage';
+
+
+function Mainpage () {
+    return(
+        <div style={{margin: '0', padding: '0'}}>
+            <MainSearchPage/>
+        </div>
+    )
+}
