ab57ad407be1a6a94fbcb6c231e8f3cf6b6df6c5
leeseooo <leeseoooo@naver.com>
2021-01-09 15:58:48 +0900

    VideoDetailPage 수정 Comment.js 추가

--- MOA/client/src/VideoDetailPage/VideoDetailPage.js
+++ MOA/client/src/VideoDetailPage/VideoDetailPage.js
@@ -1,13 +1,38 @@
 import React , {useEffect, useState} from 'react'
 import { List, Avatar, Row, Col, Tag } from 'antd'
 import moment from 'moment';
+import Comment from './Sections/Comment'
 import axios from 'axios'
 
 function VideoDetailPage(props){
     const videoId = props.match.params.videoId
     const [Video, setVideo]=useState([])
+    const [CommentLists, setCommentLists] = useState([])
 
     const videoVariable = {videoId : videoId}
+
+    useEffect(() => {
+        axios.post('/api/video/getVideoDetail', videoVariable)
+            .then(response => {
+                if (response.data.success) {
+                    setVideo(response.data.video)
+                } else {
+                    alert('Failed to get video Info')
+                }
+            })
+    
+        axios.post('/api/comment/getComments', videoVariable)
+            .then(response => {
+                if (response.data.success) {
+                    console.log('response.data.comments',response.data.comments)
+                    setCommentLists(response.data.comments)
+                } else {
+                    alert('Failed to get video Info')
+                }
+            })
+    
+    
+      }, [])
 
     return(
         <Row gutter={[16,16]}>
@@ -20,7 +45,7 @@ function VideoDetailPage(props){
                             조회수 {Video.views}회
                             <span> {moment(Video.createdAt).format("YYYY-MM-DD")} </span>
                     </List.Item>
-                    {/* <List.Item
+                    <List.Item
                         actions={[<LikeDislikes video videoId={videoId} 
                             userId={localStorage.getItem('userId')}  />,
                             <Subscriber usetTo={Video.writer._id} 
@@ -30,12 +55,20 @@ function VideoDetailPage(props){
                             avatar={<Avatar src={Video.writer && Video.writer.image} />}
                             description={Video.writer.name}
                             />
-                    </List.Item> */}
+                    </List.Item>
                         <List.Item.Meta
                             style={{ marginLeft:'3rem' }}
                             description={Video.description}
                         />
+                    {Video.startDate} ~ {Video.endDate}
+                    <Tag style={{ marginLeft:'3rem'}}>
+                        {Image.tag !== '' && (<div>{Image.tags}해시태그자리</div>)}
+                    </Tag><hr/>
+                    <Comment CommentList={CommentLists} postId={Video._id} refreshFunction={updateComment}/>
                 </div>
+            </Col>
+            <Col lg={6} xs={24}>
+                <SideVideo />
             </Col>
         </Row>
     )
