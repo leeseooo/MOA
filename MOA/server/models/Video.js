c440ffd1c9dea978bc880de5e1380a844ea2dd0b
leeseooo <leeseoooo@naver.com>
2021-01-09 12:34:23 +0900

    서버 쪽 라우터랑 모델 추가

---
+++ MOA/server/models/Video.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const videoSchema = mongoose.Schema({
+
+    writer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        maxlength: 50
+    },
+    description: {
+        type: String
+    },
+    filePath: {
+        type: String
+    },
+    category: {
+        type: String
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    duration: {
+        type: String
+    },
+    thumbnail: {
+        type: String
+    },
+    startDate: {
+        type: Date,
+        default: Date.now
+    },
+    endDate: {
+        type: Date
+    },
+    tags: [{
+        type: String
+    }]
+}, { timestamps: true })
+
+const Video = mongoose.model('Video', videoSchema);
+
+module.exports = { Video }
---
+++ MOA/server/routes/image.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const moment = require('moment');
+
+const { Image } = require("../models/Image");
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname+" ")
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        if (ext !== '.png' || ext !== '.jpg' ) {
+            return cb(res.status(400).end('png or jpg 형식만 업로드할 수 있습니다!'), false);
+        }
+        cb(null, true)
+    }
+});
+
+const upload = multer({ storage: storage }).single("file")
+
+
+//=================================
+//             User
+//=================================
+
+
+router.post("/uploadFiles", (req, res) => {
+
+    upload(req, res, err => {
+        if (err) {
+            return res.json({ success: false, err })
+        }
+        return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename })
+    })
+
+})
+
+router.get("/getImages", (req, res) => {
+
+    Image.find()
+        .populate('writer')
+        .exec((err, images) => {
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, images })
+        })
+
+});
+
+router.post("/uploadImage", (req, res) => {
+
+    const image = new Image(req.body)
+
+    image.save((err, image) => {
+        if(err) return res.status(400).json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+    console.log(req.body)
+
+});
+
+
+router.post("/getImageDetail", (req, res) => {
+
+    Image.findOne({ "_id" : req.body.imageId })
+    .populate('writer')
+    .exec((err, image) => {
+        if(err) return res.status(400).send(err);
+        res.status(200).json({ success: true, image })
+    })
+});
+
+module.exports = router;
